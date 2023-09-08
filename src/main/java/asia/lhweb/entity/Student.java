f4092e2dc1524543b86f4136da49a8317ed482bd
lh <1072344372@qq.com>
2023-09-08 16:56:43 +0800

    3 完成自定义service注解

---
+++ src/main/java/asia/lhweb/entity/Student.java
@@ -0,0 +1,53 @@
+package asia.lhweb.entity;
+
+/**
+ * @author :罗汉
+ * @date : 2023/9/8
+ */
+public class Student {
+    private Integer id;
+    private String name;
+    private String pwd;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, String pwd) {
+        this.id = id;
+        this.name = name;
+        this.pwd = pwd;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
--- src/main/java/asia/lhweb/lhspringmvc/servlet/annotation/Service.java
+++ src/main/java/asia/lhweb/lhspringmvc/servlet/annotation/Service.java
@@ -8,9 +8,9 @@ import java.lang.annotation.*;
  * @author 罗汉
  * @date 2023/09/05
  */
-@Target(ElementType.TYPE)//修饰类型
+@Target(ElementType.TYPE)//修饰类
 @Retention(RetentionPolicy.RUNTIME)//作业范围  设置runtime将来方便反射
-@Documented
+@Documented//生成文档的时候也能体现出来
 public @interface Service {
     //为什么要加呢？ 因为默认是类名+小写
     String value() default "";
